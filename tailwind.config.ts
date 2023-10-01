aaa2e4dc8d3f8daaee6b012225c24d4a4858bcf2
talentlessDeveloper <kareemope52@gmail.com>
2023-10-01 18:28:38 +0100

    added new custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,6 +23,8 @@ export default {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        hashaccent: "hsl(var(--hashnode-accent))",
+        hashforeground: "hsl(var(--hashnode-foreground))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
