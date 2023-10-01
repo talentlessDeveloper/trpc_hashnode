8dc876bf0b49c62a1186bba30f48465d37d0e594
talentlessDeveloper <kareemope52@gmail.com>
2023-10-01 17:13:49 +0100

    fix themes provider source

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,7 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
-<<<<<<< HEAD
 import { ThemeProvider } from "~/components/themes/theme-provider";
-=======
-import { ThemeProvider } from "next-themes";
->>>>>>> 69e4b992a0a2e4c6dd13cfa63cf0869359467d72
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
